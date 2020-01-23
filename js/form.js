3b07e25d9e5bc82b605e578ebaf0a868d460adca
Sheila <40238350+SelkieFaery@users.noreply.github.com>
2020-01-23 10:30:29 -0500

    Update form.js

--- js/form.js
+++ js/form.js
@@ -36,47 +36,6 @@ var count = 0;
         alert("Please use only letters A - Z for first name");
         return false;
     }
-
-	//Tests if value of city is blank
-    if (document.getElementById("city").value === "") {
-	//Allerts user to enter city
-        alert("Please enter your city");
-        return false;
-    }
-	//Tests if the value for city is anything other than a letter a-z
-    if (!city.match(/^[a-zA-Z]+$/))
-    {
-	//Alert of incorrect character is used for city
-        alert("Please use only letters A - Z for city");
-        return false;
-    }
-
-	//Tests if value for area code is blank
-    if (document.getElementById("areaCode").value === "") {
-	//Alerts user to enter area code
-        alert("Please enter your area code");
-        return false;
-    }
-	//Tests if 3 numbers are used for area code or if characters other than numbers were entered
-    if (!area.match(/(^\d{3}$)|(^\d{3}-\d{2}$)/))
-    {
-	//Alerts user to enter exactly 3 numbers for area code
-        alert("Please enter a valid 3 digit area code.");
-        return false;
-    }
-	//Tests of value of phone number is blank
-    if (document.getElementById("phoneNumber").value === "") {
-	//Alerts user to enter phone number
-        alert("Please enter your phone number");
-        return false;
-    }
-	//Tests if 37 numbers are used for phone or if characters other than numbers were entered
-    if (!phone.match(/(^\d{7}$)|(^\d{7}-\d{6}$)/))
-    {
-	//Alerts user to enter exactly 7 numbers for area code
-        alert("Please enter a valid 7 digit phone number.");
-        return false;
-    }
     if (document.getElementById("email1").value === "") {
         alert("Please enter your e-mail address");
         return false;
